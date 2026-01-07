251a53aac68a97729d999ff6779c2dea24429464
BenchiBench <38825061+BenchiBench@users.noreply.github.com>
2026-01-07 18:01:20 +0800

    更新 login.js

--- login.js
+++ login.js
@@ -2,7 +2,7 @@ const puppeteer = require('puppeteer');
 const fs = require('fs');
 const path = require('path');
 
-// 格式化时间为 "YYYY-MM-DD HH:mm:ss"
+// 格式化时间为
 function formatToISO(date) {
   return date.toISOString().split('.')[0].replace('T', ' ');
 }
